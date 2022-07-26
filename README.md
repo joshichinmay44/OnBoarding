aa7a8ae0cf18e5eb06d1c981cbdb18fa949051f5
joshichinmay44 <joshichinmay44@gmail.com>
2022-07-26 21:22:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-faf6n4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-faf6n4)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Picture from './components/carousel.js';
+import './style.css';
+
+function App() {
+  return (
+    <div>
+      Hello World!
+      <Picture />
+    </div>
+  );
+}
+
+export default App;
