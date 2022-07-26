7166f9aab2933fa76e6f5640d7e6c447081dc136
joshichinmay44 <joshichinmay44@gmail.com>
2022-07-26 22:28:23 +0530

    Carousel created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
-import {createRoot}  from 'react-dom/client';
-
+import { createRoot } from 'react-dom/client';
+//import "./responsive.css";
 import App from './App';
 
 const rootElement = document.getElementById('root');
