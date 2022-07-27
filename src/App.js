186959e44f3cb2c538a7e93729120d1d15ec2f6f
joshichinmay44 <joshichinmay44@gmail.com>
2022-07-27 10:47:06 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import './style.css';
 function App() {
   return (
     <div align="center">
-      <Card style={{ overflow: 'auto' }}>
-        <Picture />
-      </Card>
+      <Picture />
     </div>
   );
 }
