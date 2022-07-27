19b6242b2e5908c127a208cc0ce5c0501ae0ccc9
joshichinmay44 <joshichinmay44@gmail.com>
2022-07-27 10:46:22 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './style.css';
 
 function App() {
   return (
-    <div align="center" overflow="auto">
-      <Card>
+    <div align="center">
+      <Card style={{ overflow: 'auto' }}>
         <Picture />
       </Card>
     </div>
